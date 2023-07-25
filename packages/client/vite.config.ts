4d62b9414618bb1442f6e81b92b88b7b8a8094bf
muhammadatif-devsinc <muhammad.atif@devsinc.com>
2023-07-25 20:20:58 +0500

    [devops/docker-compose] ensured that client dev-server listens on all addresses :plus:

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: { port: 8080 },
+  server: { host: true, port: 8080 },
   plugins: [react()],
 });
